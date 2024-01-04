49e0bd3399e1f633bffd440bc32b9f3a471c9345
bi-cnc <137881662+bi-cnc@users.noreply.github.com>
2024-01-04 13:46:22 +0100

    Update byznys.py

--- byznys.py
+++ byznys.py
@@ -1,3 +1,5 @@
+
+
 import streamlit as st
 import yfinance as yf
 import pandas as pd
@@ -53,8 +55,14 @@ while True:
     # Převod na string s oddělovači tisíců
     data["Close"] = data["Close"].apply(lambda x: '{:,}'.format(x).replace(',', ' '))
 
-    col1, col2 = columns1.columns(2)
+    col1, col2, col3, col4 = columns1.columns(4)
     col1.metric("EUR", data['Close'].iloc[0] + " CZK", data['Change%'].iloc[0] + "%")
     col2.metric("USD", data['Close'].iloc[1] + " CZK", data['Change%'].iloc[1] + "%")
+    col3.metric("PX - Pražská burza", data['Close'].iloc[2] + " CZK", data['Change%'].iloc[2] + "%")
+    col4.metric("ČEZ", data['Close'].iloc[3] + " CZK", data['Change%'].iloc[3] + "%")
 
-
+    col1, col2, col3, col4 = columns2.columns(4)
+    col1.metric("Ropa Brent", data['Close'].iloc[4] + " $", data['Change%'].iloc[4] + "%")
+    col2.metric("S&P 500", data['Close'].iloc[5] + " $", data['Change%'].iloc[5] + "%")
+    col3.metric("NASDAQ", data['Close'].iloc[7] + " $", data['Change%'].iloc[7] + "%")
+    col4.metric("Bitcoin", data['Close'].iloc[6] + " $", data['Change%'].iloc[6] + "%")
